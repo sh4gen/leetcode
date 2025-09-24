8a2113e5c79907ee9eac4124cbbd785b75c0710b
sh4gen <keylogershagen@gmail.com>
2025-09-24 13:04:54 +0300

    first commit

--- 14.py
+++ 14.py
@@ -2,8 +2,14 @@ class Solution(object):
     def longestCommonPrefix(self, strs):
         if not strs:
             return ""
+        
         shortest = min(strs, key=len)
-        for i, character in enumerate(shortest):
+        
+        if shortest == "":
+            return ""
+        
+        for i, ch in enumerate(shortest):
             for other in strs:
-                if other[i] != character:
+                if other[i] != ch:
                     return shortest[:i]
+        return shortest
